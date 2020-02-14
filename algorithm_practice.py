fa7a793f0f1d2d69665fa96cf7187fb1a1bfa3a8
Jessica <jessica.ho.jh@gmail.com>
2020-02-14 11:38:01 -0800

    solved swap elements

--- algorithm_practice.py
+++ algorithm_practice.py
@@ -1139,3 +1139,23 @@ print(swap_elements2(['G', 'B', 'R', 'R', 'B', 'R', 'G']))
 print(swap_elements2(['R', 'R', 'R', 'R', 'B', 'B', 'B']))
 print(swap_elements2(['G', 'B', 'B', 'R', 'B', 'G', 'R']))
 print(swap_elements2(['G', 'B', 'B', 'R', 'B', 'G', 'B']))
+
+print("BREAK")
+
+# This problem was asked by Google.
+# The power set of a set is the set of all its subsets. Write a function that, given a set, generates 
+# its power set.
+# For example, given the set {1, 2, 3}, it should 
+# return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+# You may also use a list or array to represent a set.
+
+# def power_set(input_set):
+
+#     answer = set()
+
+#     # for num in input_set:
+
+#     return set({1,2}, {2,1})
+
+
+print(power_set({1, 2, 3}))
