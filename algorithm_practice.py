bc5a41da2a3cc5f932af5dd1691aec801c49fe4f
Jessica <jessica.ho.jh@gmail.com>
2020-02-25 16:23:13 -0800

    solved max sum of contiguout subarray

--- algorithm_practice.py
+++ algorithm_practice.py
@@ -1158,4 +1158,25 @@ print("BREAK")
 #     return set({1,2}, {2,1})
 
 
-print(power_set({1, 2, 3}))
+# print(power_set({1, 2, 3}))
+
+# Given an array of numbers, find the maximum sum of any contiguous subarray of the array.
+# For example, given the array [34, -50, 42, 14, -5, 86], the maximum sum would be 137, since we 
+# would take elements 42, 14, -5, and 86.
+# Given the array [-5, -1, -8, -9], the maximum sum would be 0, since we would not take any elements.
+# Do this in O(N) time.
+
+def max_sum(arr):
+
+    max_current = arr[0]
+    max_global = arr[0]
+
+    for i in range(1,len(arr)):
+        max_current = max(arr[i], max_current + arr[i])
+        if max_current > max_global:
+            max_global = max_current
+            
+    return max_global
+
+print(max_sum([34, -50, 42, 14, -5, 86])) # 137
+print(max_sum([-5, -1, -8, -9])) # 0
